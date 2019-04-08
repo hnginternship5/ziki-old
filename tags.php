8b3108090c33d8192d8b2de61e3df12ce27a2de3
drumzminister <ishukpong418@gmail.com>
2019-04-08 20:54:05 +0100

    made tag variable have a null value when accessed from pages other than the tags page

--- tags.php
+++ tags.php
@@ -1,5 +1,7 @@
 <?php
 // Check if the server request method is, and the request URL is tags
 if($_SERVER['REQUEST_METHOD'] == "GET" && $_SERVER['PHP_SELF'] == "/tags.php"){
-    $tag = isset($_GET['t']) ? $_GET['t'] : null ;
+    $tag = isset($_GET['t']) ? $_GET['t'] : NULL ;
+}else{
+    $tag = NULL;
 }
