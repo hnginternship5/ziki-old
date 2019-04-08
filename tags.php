0081ace42d97003f7f35f011380f9ad718deaad7
drumzminister <ishukpong418@gmail.com>
2019-04-08 20:46:40 +0100

    implemented tags retrieval when GET request is pointed to tags

---
+++ tags.php
@@ -0,0 +1,5 @@
+<?php
+// Check if the server request method is, and the request URL is tags
+if($_SERVER['REQUEST_METHOD'] == "GET" && $_SERVER['PHP_SELF'] == "/tags.php"){
+    $tag = isset($_GET['t']) ? $_GET['t'] : null ;
+}
