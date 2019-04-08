e727ea3e10d2c8cb667f81ff445ed4f4f09f746f
ofega <fegaeze@gmail.com>
2019-04-08 22:39:53 +0100

    getting updates

--- feeds.php
+++ feeds.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/rssfeed-light.css" />
     <link
       rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+      href="https://use.fontawesome.com/releases/v5.5.0/css/rssfeeds-dark.css"
       integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
       crossorigin="anonymous"
     />
